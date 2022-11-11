9b7df7d8a2c8b7de9cb4082e4f2bd7abed77d928
Ashith Prakas <ashith.prakas@soti.net>
2022-11-11 16:24:04 +0530

    properties panel value retireval complete

--- src/app/properties-panel/properties-panel.component.ts
+++ src/app/properties-panel/properties-panel.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertiesPanelComponent implements OnInit {
   constructor() {}
-
+  strokecolor: string = '#9a3c3c';
+  fillcolor: string = '#9a3c3c';
+  strokeWidth: string = '5';
+  objectAngle: string = '0';
   ngOnInit(): void {}
+  onStrokeColorChange(value: string) {
+    console.log(value);
+  }
+  onStrokeWidthChange(value: string) {
+    console.log(value);
+  }
+  onFillColorChange(value: string) {
+    console.log(value);
+  }
+  onAngleChange(value: string) {
+    console.log(value);
+  }
 }
